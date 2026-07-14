94c639dbab65a3d7f9aed15aafbb3896308cb5ce
mbkilroy83 <matt.kilroy83@gmail.com>
2026-07-14 14:20:31 -0600

    feat: Built Authenticated Listing POST Route

---
+++ backend/controllers/listingController.js
@@ -0,0 +1,28 @@
+const Listing = require('../models/Listing');
+
+//Attempt to create a new listing
+const createListing = async (req, res) => {
+  try {
+    const { items, pickupInstructions } = req.body;
+
+    //Create the new listing
+    const newListing = await Listing.create({
+      donor: req.user.id, 
+      items,
+      pickupInstructions
+    });
+
+    //Return the created listing with a success status
+    res.status(201).json(newListing);
+  } 
+  
+  //Return an error status if lsiting fails
+  catch (error) {
+    console.error('Error creating listing:', error);
+    res.status(500).json({ message: 'Server error. Could not create listing.', error: error.message });
+  }
+};
+
+module.exports = {
+  createListing,
+};
