9408ef693265b22b5a476d7064b94adbc0a974ed
pguf12345 <sphetr2@ufl.edu>
2026-07-14 22:43:28 +0000

    keep user model import style

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   accountType: {
@@ -35,4 +35,4 @@ const userSchema = new mongoose.Schema({
   zipCode: String,
 });
 
-module.exports = mongoose.model("User", userSchema);
+export default mongoose.model("User", userSchema);
