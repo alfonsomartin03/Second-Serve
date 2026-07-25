9b36c8309c0d90b325897ca76da1493c23a1ec8b
pguf12345 <sphetr2@ufl.edu>
2026-07-25 09:37:12 -0700

    add notification api

---
+++ backend/routes/notificationRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { protect } = require("../middleware/authMiddleware");
+const {
+    getNotifications,
+    markNotificationsRead
+} = require("../controllers/notificationController");
+
+const router = express.Router();
+
+router.get("/", protect, getNotifications);
+router.patch("/read", protect, markNotificationsRead);
+
+module.exports = router;
